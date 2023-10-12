312085e32ae5f1cc6a9c840dfeef853d248d9b14
Tello <pateltanaka22@live.com>
2023-10-12 12:54:24 +0200

    profile confronller

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -23,18 +23,53 @@ exports.createProfile = (req, res) => {
     });
 };
 
+
+const  paginatedResults = async (model , req , options) => {
+
+    const page = parseInt(req.query.page);
+    const limit =10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const result = {
+
+    };
+// change model.length to model.countDocuments() because you are counting directly from mongodb
+    if (endIndex < (await model.countDocuments().exec())) {
+      result.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    try {
+//       .limit(limit).skip(startIndex) replaced the slice method because
+//       it is done directly from mongodb and they are one of mongodb methods
+      result.content = await model.find(options).limit(limit).skip(startIndex).sort({createdAt:-1});
+      if(result.content.length < limit){
+          result.nextPage = false;
+          result.total = await model.countDocuments()
+      }else{
+          result.nextPage = true;
+          result.total = await model.countDocuments()
+      }
+
+      return result;
+    } catch (e) {
+       return e;
+    }
+
+}
 // Get paginated profiles
-exports.getProfiles = (req, res) => {
+exports.getProfiles = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
 
-  Profile.find()
-    .skip((page - 1) * limit)
-    .limit(limit)
-    .then((profiles) => {
-      res.json(profiles);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Failed to get profiles' });
-    });
+  const   paginatedProfiles = await paginatedResults(Profile ,req);
+
+  res.status(200).json(paginatedProfiles);
 };
