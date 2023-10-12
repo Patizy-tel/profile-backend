fdb190f9cd78f3ab64b28ba72765a618cdd3d612
Tello <pateltanaka22@live.com>
2023-10-12 11:45:21 +0200

    index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const db = require('./db');
+const cors = require("cors");
 const profileRoutes = require('./routes/profileRoutes');
 
 dotenv.config();
@@ -10,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api', profileRoutes);
 
 app.listen(port, () => {
